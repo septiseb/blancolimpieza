2d2607e9225da6ff13e176bbd17a0912a2a0874a
Sebastian Septien <sebsepdus@hotmail.com>
2023-09-17 21:37:49 -0600

    Adding the SEO to validate page

---
+++ src/components/SEO.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function SEO() {
+  return (
+    <>
+        <meta  /> 
+<meta name="google-site-verification" content="38UvtEOkIvDZnNnLzJSdPosB8MMJwKv1LFHGAnjDBP0" />
+       
+    </>
+  )
+}
